aa5e2d8b30d409aebad57c90f2f2f6e9ca84e1b3
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-04-16 16:43:23 -0400

    added Google web scraping functionality to main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, request
+import requests
+import bs4
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def evaluate():
   if request.method == "GET":
-    return '<p>Hello World!</p>'
+    search_query = 'when+was+the+constitution+of+the+united+states+signed'
+    url = 'https://google.com/search?q=' + search_query
+    result = requests.get(url)
+    soup = bs4.BeautifulSoup(result.text, "html.parser")
+    print(soup)
+    return str(soup)
 
 if __name__ == '__main__':
   app.debug = True
