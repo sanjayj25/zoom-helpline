0b24457110c3ccb1d196f8db937f224e582a91c1
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-04-17 07:40:48 -0400

    created speech-to-text conversion function

--- speech_conversion.py
+++ speech_conversion.py
@@ -1,8 +1,20 @@
-import speech_recognition as s_r
-print(s_r.__version__) # just to print the version not required
-r = s_r.Recognizer()
-my_mic = s_r.Microphone(device_index=0) #my device index is 1, you have to put your device index
-with my_mic as source:
-    print("Say now!!!!")
-    audio = r.listen(source) #take voice input from the microphone
-print(r.recognize_google(audio)) #to print voice into text
+import os
+from google.cloud import speech
+
+def speech_to_text(filename):
+  os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'speech_to_text_key.json'
+
+  with open(filename, 'rb') as f:
+    byte_data = f.read()
+  audio = speech.RecognitionAudio(content=byte_data)
+  config = speech.RecognitionConfig(
+    sample_rate_hertz=48000,
+    enable_automatic_punctuation=True,
+    language_code='en-US'
+  )
+  speech_client = speech.SpeechClient()
+  response = speech_client.recognize(config=config, audio=audio)
+  transcription = ''
+  for result in response.results:
+    transcription += result.alternatives[0].transcript
+  return transcription
